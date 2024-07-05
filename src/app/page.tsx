14caa4751deb6ebf6363e824d0c508ed2dfb95d7
techswarn <tech.swarns@gmail.com>
2024-07-05 09:59:25 +0530

    code update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,12 @@ export default function Index() {
       <div className="wrapper">
         <div className="container">
           <h1>Sample App</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            facere minima, et quaerat suscipit vitae similique, fuga rem
+            corporis, autem consectetur accusamus. Vero, nobis! Earum nostrum
+            voluptates quia!
+          </p>
         </div>
       </div>
     </div>
